f133fdcb687c010db18b29d5ea34b8197a0e9cc1
blueju <blueju@foxmail.com>
2019-09-07 20:49:38 +0800

    配置区和配置弹窗表单的编辑/新增功能完成

--- src/components/ConfigArea.vue
+++ src/components/ConfigArea.vue
@@ -13,9 +13,9 @@
         ></a-button>
         <a-tag
           color="#108ee9"
-          v-for="(secondItem,index) in item.list"
-          :key="index"
-          @click="editConfig(index,item,secondItem)"
+          v-for="(secondItem,secondIndex) in item.list"
+          :key="secondIndex"
+          @click="editConfig(index,secondIndex,item,secondItem)"
         >{{ secondItem.brand }} {{ secondItem.model }} {{ secondItem.price }}</a-tag>
       </a-card>
     </section>
@@ -24,22 +24,24 @@
       <a-modal
         class="configDialog"
         :title="configDialogData.title"
-        v-model="showConfigDialog"
-        @ok="closeConfigDialog"
+        :maskClosable="false"
+        :visible="showConfigDialog"
+        @ok="submitConfigForm"
+        @cancel="configDialogFormCancel"
       >
         <!-- 表单行内块布局 Horizontal -->
         <a-form onSubmit>
           <a-form-item label="品牌">
-            <a-input placeholder="input placeholder" />
+            <a-input v-model="configDialogData.formData.brand" />
           </a-form-item>
           <a-form-item label="型号">
-            <a-input placeholder="input placeholder" />
+            <a-input v-model="configDialogData.formData.model" />
           </a-form-item>
           <a-form-item label="价格">
-            <a-input placeholder="input placeholder" />
+            <a-input v-model="configDialogData.formData.price" />
           </a-form-item>
           <a-form-item label="链接">
-            <a-input placeholder="input placeholder" />
+            <a-input v-model="configDialogData.formData.purchaseLinks" />
           </a-form-item>
         </a-form>
       </a-modal>
@@ -88,9 +90,16 @@ export default {
       ],
       // 配置弹窗的配置数据
       configDialogData: {
-        index: 0, // 默认为1，即CPU
+        index: 0, // 一级索引，代表配件，默认为0，即CPU
+        secondIndex: 0, // 二级索引，代表具体配件，默认为0
+        type: "",
         title: "",
-        formData: {}
+        formData: {
+          brand: "",
+          model: "",
+          price: "",
+          purchaseLinks: ""
+        }
       }
     };
   },
@@ -101,9 +110,11 @@ export default {
      * item
      */
     addConfig(index, item, secondItem) {
+      debugger;
       // 打开配置弹窗对话框
       this.showConfigDialog = true;
-      this.configDialogData.config = index;
+      this.configDialogData.index = index;
+      this.configDialogData.type = "add";
       this.configDialogData.title = `添加 ${item.name} 配置`;
     },
     /**
@@ -111,15 +122,47 @@ export default {
      * index  索引
      * item
      */
-    editConfig(index, item) {
+    editConfig(index, secondIndex, item, secondItem) {
+      debugger;
       // 打开配置弹窗对话框
       this.showConfigDialog = true;
-      this.configDialogData.config = index;
+      this.configDialogData.index = index;
+      this.configDialogData.secondIndex = secondIndex;
+      this.configDialogData.type = "edit";
       this.configDialogData.title = `编辑 ${item.name} 配置`;
+      this.configDialogData.formData = JSON.parse(JSON.stringify(secondItem));
     },
-    // 关闭配置弹窗对话框
-    closeConfigDialog() {
+    // 提交配置弹窗表单
+    submitConfigForm() {
+      debugger;
+      let index = this.configDialogData.index;
+      let secondIndex = this.configDialogData.secondIndex;
+      switch (this.configDialogData.type) {
+        case "add":
+          // 将配置表单数据添加到
+          this.config[index].list.push(
+            JSON.parse(JSON.stringify(this.configDialogData.formData))
+          );
+          break;
+        case "edit":
+          this.config[index].list[secondIndex] = JSON.parse(
+            JSON.stringify(this.configDialogData.formData)
+          );
+          break;
+      }
+
+      // 清空配置弹窗表单
+      for (let i in this.configDialogData.formData) {
+        this.configDialogData.formData[i] = "";
+      }
       this.showConfigDialog = false;
+    },
+    // 取消配置弹窗表单，清空
+    configDialogFormCancel() {
+      this.showConfigDialog = false;
+      for (let i in this.configDialogData.formData) {
+        this.configDialogData.formData[i] = "";
+      }
     }
   }
 };
